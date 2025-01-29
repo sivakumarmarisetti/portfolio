49d3a1e46c0f8e64195677a2017d273a530ce662
sivakumarmarisetti <marisettisivakumar2000@gmail.com>
2025-01-29 13:11:18 +0530

    updated loading page and homepage

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ window.addEventListener('load', () => {
   const loading = document.getElementById('loading');
   setTimeout(() => {
     loading.style.display = 'none';
+    document.body.classList.remove('loading');
   }, 3000); // Adjust the delay to match the animation duration
+  document.body.classList.add('loading');
 });
