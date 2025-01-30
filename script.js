b2309cb6bce2ef1d1261330b286599d361791ddc
sivakumarmarisetti <marisettisivakumar2000@gmail.com>
2025-01-30 12:14:44 +0530

    loading page centerd in pixel 6a

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ window.addEventListener('load', () => {
   }, 3000); // Adjust the delay to match the animation duration
   document.body.classList.add('loading');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// JavaScript for scroll animations
+const sections = document.querySelectorAll('section');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
